416c9234a9a34c6ec4415812236ea5ae46d2f980
Louie-Cipher <louie.spena@outlook.com>
2023-03-02 09:35:07 -0300

    change component on screen resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './Components/Footer';
+import MobileTable from './Components/MobileTable';
 import Table from './Components/Table';
 import Title from './Components/Title';
 
 export default function App() {
+	const detectScreenSize = () => {
+		const width = window.innerWidth;
+		return width < 768;
+	};
+
+	const [isMobile, setIsMobile] = useState(detectScreenSize());
+
+	window.addEventListener('resize', () => setIsMobile(detectScreenSize()));
+
 	return (
 		<div className='App'>
 			<Title />
-			<Table />
+			{isMobile ? <MobileTable /> : <Table />}
 			<Footer />
 		</div>
 	);
